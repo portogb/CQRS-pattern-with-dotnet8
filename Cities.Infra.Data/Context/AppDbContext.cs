a9120292b731a8656258f26d37db7a199f5e43ca
portogb <porto.g4briel@gmail.com>
2024-02-14 23:53:17 -0300

    feat: add db context

---
+++ Cities.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Cities.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cities.Infra.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
