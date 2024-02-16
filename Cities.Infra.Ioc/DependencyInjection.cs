a68180a6410f4ce2a52561e5e852bd3b75abc6bc
portogb <porto.g4briel@gmail.com>
2024-02-15 23:37:44 -0300

    feat: create ioc project

---
+++ Cities.Infra.Ioc/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cities.Infra.Ioc
+{
+    public static class DependencyInjection
+    {
+        //public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
+        //{}
+    }
+}
