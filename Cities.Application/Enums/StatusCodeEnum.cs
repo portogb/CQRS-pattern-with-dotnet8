421454cf3ecb452253eb450b73d72a614945d6bf
portogb <porto.g4briel@gmail.com>
2024-02-19 23:14:10 -0300

    feat: create enum status code

---
+++ Cities.Application/Enums/StatusCodeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cities.Application.Enums
+{
+    public enum StatusCodeEnum
+    {
+        Validation = 422,
+        Success = 200,
+        BadRequest = 400,
+        InternalServerError = 500
+    }
+}
