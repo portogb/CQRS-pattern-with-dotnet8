9d0923884e7faf893a1c072de8405eb5d9af8d3b
portogb <porto.g4briel@gmail.com>
2024-03-14 17:16:14 -0300

    feat: add test configuration

---
+++ Cities.Tests/Infra.Data/AppDbContextTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cities.Tests.Infra.Data
+{
+    public class AppDbContextTest : AppTestContext
+    {
+        [Fact]
+        public async Task DatabaseIsAvaiableAndCanBeConnectedTo() => 
+            Assert.True(await _context.Database.CanConnectAsync());
+    }
+}
---
+++ Cities.Tests/Infra.Data/AppTestContext.cs
@@ -0,0 +1,32 @@
+﻿using Cities.Infra.Data.Context;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cities.Tests.Infra.Data
+{
+    public class AppTestContext : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        protected readonly AppDbContext _context;
+
+        protected AppTestContext()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _context = new AppDbContext(options);
+            _context.Database.EnsureCreated();
+        }
+
+        public void Dispose() => _connection.Close();
+    }
+}
